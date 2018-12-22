c6600ea17fb0c5304d9ed5bf7b8fb112fd419b35
rawazkader85 <rkade103@uottawa.ca>
2018-12-22 13:42:48 -0500

    Added spawners, health, and death particles to the attackers. Added a cactus defender as well as zucchini projectile.

---
+++ Assets/Scripts/Attacker.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attacker : MonoBehaviour {
+
+    [Range(0f, 5f)] float currentSpeed = 1f;
+
+    public void SetMovementSpeed(float speed)
+    {
+        currentSpeed = speed;
+    }
+
+	void Update () {
+        transform.Translate(Vector2.left * currentSpeed * Time.deltaTime);
+	}
+
+
+}
